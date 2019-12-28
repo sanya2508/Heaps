2f0920159517aaf4ec9b8daf95737643d88e792c
Sanya Kumari <55986045+sanya2508@users.noreply.github.com>
2019-12-28 23:10:52 +0530

    Create deleteAnyNodeApartFromRoot.cpp

---
+++ deleteAnyNodeApartFromRoot.cpp
@@ -0,0 +1,6 @@
+/* update the value of node to be deleted with the max possible priority value (in case of max heap) and with min possible priority value (in case of min heap)
+perculate it up in order until it becmoes the root node
+swap root with the last node
+pop out the last node
+apply downheapify to make the heap
+update the array */
